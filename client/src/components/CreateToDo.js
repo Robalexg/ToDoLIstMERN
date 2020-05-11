ed8fbc3f78dd3bcf28001e8bbf2ddcb7809fecf2
Robert <Alexg7070@yahoo.com>
2020-05-11 18:53:04 -0500

    Fixed form and radio button data

--- client/src/components/CreateToDo.js
+++ client/src/components/CreateToDo.js
@@ -1,36 +1,59 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Row,Col,Form,Button} from 'react-bootstrap'
 
 
 
 const CreateToDo = () => {
 
-	const submit = (e) => {
+	const [desc,setDesc] = useState('');
+	const [resp,setResp] = useState('');
+	const [pri,setPri] = useState('');
+
+	const onDescChange = (e) => {
+		setDesc(e.target.value)
+	}
+
+	const onRespChange = (e) => {
+		setResp(e.target.value)
+	}
+
+	const onPriChange = (e) => {
+		setPri(e.target.value)
+	}
+
+
+	const onSubmit = (e) => {
 		e.preventDefault()
-		console.log(e.target)
+		console.log("Description",desc)
+		console.log("Responible",resp)
+		console.log("priority",pri)
+
+		setDesc('')
+		setResp('')
+		setPri('')
 	}
 	return (
 		<Row>
 			<Col className='py-3'>
 				<h1>Create a New To Do</h1>	
-				<Form onSubmit={submit}>
+				<Form onSubmit={onSubmit}>
 					<Form.Group className='mb-4'>
 						<Form.Label>Description:</Form.Label>
-						<Form.Control ></Form.Control>
+						<Form.Control value ={desc} onChange={onDescChange}></Form.Control>
 						
 					</Form.Group>
 					<Form.Group className='mb-4'>
 						<Form.Label>Responible:</Form.Label>
-						<Form.Control ></Form.Control>
+						<Form.Control value ={resp} onChange={onRespChange} ></Form.Control>
 					</Form.Group>
 
 					<fieldset>
-						<Form.Check inline type='radio' label='Low' id='low' name='priority'></Form.Check>
-						<Form.Check inline type='radio' label='Medium' id='medium' name='priority'></Form.Check>
-						<Form.Check inline type='radio' label='High' id='high' name='priority'></Form.Check>
+						<Form.Check onChange={onPriChange} checked={pri === 'low'} value='low' inline type='radio' label='Low' id='low' name='low'></Form.Check>
+						<Form.Check onChange={onPriChange} checked={pri === 'medium'} value='medium' inline type='radio' label='Medium' id='medium' name='medium'></Form.Check>
+						<Form.Check onChange={onPriChange} checked={pri === 'high'} value='high' inline type='radio' label='High' id='high' name='high'></Form.Check>
 					</fieldset>
 					<Form.Group>
-						<Button  type ='submit'variant='primary' className='my-4'>Create To Do</Button>
+						<Button type ='submit'variant='primary' className='my-4'>Create To Do</Button>
 					</Form.Group>
 				</Form>
 
